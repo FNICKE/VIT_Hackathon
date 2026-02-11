c6d1f348c5f8f4f950fb4884fb9ffabe34d0139f
RJDEVMAN <219867181+RJDEVMAN@users.noreply.github.com>
2026-02-11 16:59:55 +0530

    Re-committing

--- backend/creating_private_key.py
+++ backend/creating_private_key.py
@@ -1,9 +1,6 @@
 # I have created this file for generating private key and address.
 # While merging, DO NOT Run this file again.
 # Algorand details aare pasted here itself for final testing.
-# Algorand Address:- '4BG6P2JDQXYWFJ4ERCSY5JINE6DSN55NXUJS5CU4Z27MQCRHXXGTUAVT24'.
-# Algorand Private Key:- '/xMHpjkAH76HpGeqVLpdmFPHhh9hug5DCCiAA9rkpYzgTefpI4XxYqeEiKWOpQ0nhyb3rb0TLoqczr7ICie9zQ=='
-# Copy them and paste in .env file as PRIVATE_KEY and use the address for testing.
 from algosdk import account
 
 private_key, address = account.generate_account()
